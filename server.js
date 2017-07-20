af804f75a96d49936a283cc4ff86228d9fb74c46
itsandromeda <itsandromeda@gmail.com>
2017-07-20 13:54:53 -0500

    Init

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+    app = express();
+
+app.use('/', express.static('public'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000');
+});
