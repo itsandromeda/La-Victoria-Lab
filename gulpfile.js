971fde500f823a9ac4b33d94849896c358c0113f
Mitchell Rodríguez <mitrodle@gmail.com>
2017-07-21 01:12:05 -0500

    Gulp (Fixed) + Data talks

--- gulpfile.js
+++ gulpfile.js
@@ -1,110 +1,110 @@
-/*global require*/
-/*jshint esversion: 6*/
-var bodyParser = require('body-parser'),
-    browserSync = require('browser-sync').create(),
-    gulp = require('gulp'),
-    browserify = require('gulp-browserify'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    config = {
-        source: './src/',
-        dist: './public/',
-        modules: './node_modules/'
-    },
-    paths = {
-        assets: 'assets/',
-        html: '**/*.html',
-        jpg: 'img/**/*.jpg',
-        png: 'img/**/*.png',
-        fonts: 'fonts/**/*.ttf',
-        SASS: 'scss/**/*.scss',
-        mainSASS: 'scss/main.scss',
-        js: 'js/**/*.js',
-        bootstrap: 'bootstrap/dist/'
-    },
-    sources = {
-        assets: config.source + paths.assets,
-        html: config.source + paths.html,
-        jpg: config.source + paths.assets + paths.jpg,
-        png: config.source + paths.assets + paths.png,
-        fonts: config.source + paths.assets + paths.fonts,
-        SASS: config.source + paths.assets + paths.SASS,
-        mainSASS: config.source + paths.assets + paths.mainSASS,
-        js: config.source + paths.assets + paths.js,
-        rootJS: config.source + paths.assets + "js/app.js",
-        rootSass: config.source + paths.assets + paths.mainSass,
-        bootstrapCSS: config.modules + paths.bootstrap + 'css/bootstrap.min.css',
-        bootstrapJS: config.modules + paths.bootstrap + 'js/bootstrap.min.js'
-    };
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
 
-gulp.task('html', () => {
-    gulp.src(sources.html)
-        .pipe(gulp.dest(config.dist));
+
+var config = {
+  source: './src/',
+  dist: './public/'
+};
+
+var paths = {
+  assets: "assets/",
+  html: "**/*.html",
+  json: '**/*.json',
+  img: "img/**",
+  js:"js/*.js",
+
+  components: "components/**",
+  utils: "utils/**",
+
+  css: "css",
+  sass: "scss/**/*.scss",
+  mainSass: "scss/main.scss",
+  mainJS:"js/**/*.js"
+};
+
+var sources = {
+  assets: config.source + paths.assets,
+  html: config.source + paths.html,
+  json: config.source + paths.json,
+  img: config.source + paths.assets + paths.img,
+  js: config.source + paths.assets +  paths.js,
+  sass: config.source + paths.assets + paths.sass,
+  rootSass: config.source + paths.assets + paths.mainSass,
+  rootJS: config.source + paths.assets + paths.mainJS
+};
+
+gulp.task('html', ()=> {
+  gulp.src(sources.html)
+    .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('images', () => {
-    gulp.src([sources.jpg, sources.png])
-        .pipe(gulp.dest(config.dist + 'img'));
+gulp.task('json', () => {
+    gulp.src(sources.json)
+    .pipe(gulp.dest(config.dist));
+})
+
+gulp.task('img', ()=> {
+  gulp.src(sources.img)
+    .pipe(gulp.dest(config.dist + paths.assets + "img"));
 });
 
-gulp.task('fonts', () => {
-    gulp.src(sources.fonts)
-        .pipe(gulp.dest(config.dist + 'fonts'));
+gulp.task('sass', ()=> {
+  gulp.src(sources.rootSass)
+    .pipe(sass({
+      outputStyle: "expanded"
+    })
+      .on ("error", sass.logError))
+    .pipe(gulp.dest(config.dist + paths.assets + "css"));
 });
 
-gulp.task('sass', () => {
-    gulp.src([sources.bootstrapCSS, sources.mainSASS])
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on("error", sass.logError))
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest(config.dist));
+gulp.task('js', ()=> {
+  gulp.src(sources.rootJS)
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest(config.dist + paths.assets + "js"));
 });
 
-
-gulp.task('js', () => {
-    gulp.src([sources.bootstrapJS, sources.js, sources.rootJS])
-        .pipe(concat(sources.rootJS))
-        .pipe(browserify())
-        .pipe(rename('bundle.js'))
-        .pipe(gulp.dest(config.dist));
+gulp.task("html-watch", ["html"], function (done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('img-watch', ['images'], (done) => {
+gulp.task("json-watch", ["json"], function (donde) {
     browserSync.reload();
     done();
+})
+
+gulp.task("img-watch", ["img"], function (done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('fonts-watch', ['fonts'], (done) => {
-    browserSync.reload();
-    done();
+gulp.task("js-watch", ["js"], function (done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('sass-watch', ['sass'], (done) => {
-    browserSync.reload();
-    done();
+gulp.task("sass-watch", ["sass"], function (done) {
+  browserSync.reload();
+  done();
 });
 
-gulp.task('js-watch', ['js'], (done) => {
-    browserSync.reload();
-    done();
-});
+gulp.task("serve", ()=> {
+  browserSync.init({
+    server: {
+      baseDir: config.dist
+    }
+  });
 
-gulp.task('html-watch', ['html'], (done) => {
-    browserSync.reload();
-    done();
-});
+  gulp.watch(sources.html, ["html-watch"]);
+  gulp.watch(sources.json, ["json-watch"]);
+  gulp.watch(sources.img, ["img-watch"]);
+  gulp.watch(sources.fonts, ["fonts-watch"]);
+  gulp.watch(sources.sass, ["sass-watch"]);
+  gulp.watch(sources.rootJS, ["js-watch"]);
 
-gulp.task('serve', () => {
-    browserSync.init({
-        server: {
-            baseDir: config.dist
-        }
-    });
-    gulp.watch(sources.html, ['html-watch']);
-    gulp.watch([sources.SASS], ['sass-watch']);
-    gulp.watch([sources.jpg, sources.png], ['img-watch']);
-    gulp.watch([sources.fonts], ['fonts-watch']);
-    gulp.watch(sources.js, ['js-watch']);
 });
---
+++ public/assets/js/bundle.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const render = (root) => {
+
+	root.empty();
+
+	const wrapper = $('<div class="wrapper"></div>');
+
+	const update = function() {
+		render(root);
+	}
+
+	if(state.screen == null) {
+		wrapper.append(schedule(update))
+	}
+
+	root.append(wrapper);
+
+}
+
+const state = {
+	screen: null,
+	data: null,
+	id: null
+};
+
+$( _ => {
+
+	getJSON('test.json', (err, json) => {
+
+		if(err){ return alert(err.message);}
+		state.data = json.charlas
+		console.log(state.data)
+
+		const root = $('#root');
+		render(root);
+
+	});
+});
+
+const schedule = (update) => {
+    const container = $(`<header class="container-fluid">
+                            <nav class="row">
+                                <h1>MYFEST</h1>
+                                <i class="fa fa-bars"></i>
+                            </nav>
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <img src="" alt="avatar">
+                                </div>
+                                <div class="col-xs-9">
+                                    <h3>Ana Izquierdo</h3>
+                                    <p>Oeschle</p>
+                                </div>
+                            </div>
+                         </header>
+                         <section class="schedule-wrap">
+                            <div class="container">
+                                <div class="row">
+                                    
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <p>15:00</p>
+                                        <p>What is Digital Transformation</p>
+                                        <p>Mike Bracken</p>
+                                        <div>
+                                            <i class="fa fa-location"></i>
+                                            <p>Carpa Mixta</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12">
+                                        <p>15:00</p>
+                                        <p>What is Digital Transformation</p>
+                                        <p>Mike Bracken</p>
+                                        <div>
+                                            <i class="fa fa-location"></i>
+                                            <p>Carpa Mixta</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-12">
+                                        <p>15:00</p>
+                                        <p>What is Digital Transformation</p>
+                                        <p>Mike Bracken</p>
+                                        <div>
+                                            <i class="fa fa-location"></i>
+                                            <p>Carpa Mixta</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         </section>
+                         <footer>
+                            <button class="text-center">CAMBIAR MIS CHARLAS REGISTRADAS</button>
+                         </footer>`);
+
+    return container;
+};
+'use strict';
+
+const getJSON = (url, cb) => {
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+
+    if (xhr.status !== 200) {
+      return cb(new Error('Error loading JSON from ' + url + '(' + xhr.status + ')'));
+    }
+
+    cb(null, xhr.response);
+    
+  });
+
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+  xhr.send();
+
+};
